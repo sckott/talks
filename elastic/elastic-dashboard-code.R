74140328f30415cabea8e2edd40a4e88b6786ff9
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-21 11:28:54 -0700

    started phos16 talk

--- elastic/elastic-dashboard-code.R
+++ elastic/elastic-dashboard-code.R
@@ -1,4 +1,13 @@
+rmarkdown::run("elastic-dashboard.Rmd")
+
+library("elastic")
+elastic::connect()
+index_delete("mtcars")
+
 mtcars$cyl <- mtcars$cyl + 3
+docs_bulk(mtcars, "mtcars")
+count('mtcars')
+
 mtcars$cyl <- mtcars$cyl + 20
 docs_bulk(mtcars, "mtcars")
 count('mtcars')
